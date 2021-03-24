7c2cb769b547832a69fafafb725f7c2a9f60fc5b
Neel Smith <nsmith@Technites.local>
2021-03-24 07:51:01 -0400

    Rm commented-out cruft

--- test/test_litgreek.jl
+++
@@ -1,9 +0,0 @@
-using Orthography
-using Test
-
-
-@testset "Test supertype of orthography" begin
-    greek = literaryGreek()
-    @test typeof(greek) == LiteraryGreek
-    @test supertype(LiteraryGreek) == OrthographicSystem
-end
