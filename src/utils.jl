d7993bbd0eaf0b0d3e75b72e08a5ad91a48d8e21
Neel Smith <dnsmith.neel@gmail.com>
2022-12-24 11:31:33 -0500

    Add util functions for stripping off front of strings

--- src/utils.jl
+++ src/utils.jl
@@ -2,6 +2,53 @@
 
 """
 $(SIGNATURES)
+Remove all leading characters belong to list in `arr`,
+and collect them.  This is useful to collect punctuation
+from a token, for example.
+"""
+function collecthead(s::AbstractString, arr, stripped=[])
+    if isempty(s)
+        join(stripped,"")
+    else 
+        firstch = string(s[1])
+        @info("Look for $(firstch) in $(arr)")
+        if occursin(firstch, arr)
+            push!(stripped, firstch)
+            trimmed = s[2:end]
+            collecthead(trimmed, arr, stripped)
+        else
+            join(stripped,"")
+        end
+    end
+end
+
+
+"""
+Remove all leading characters belong to list in `arr`,
+and return the trimmed string.  
+
+$(SIGNATURES)
+
+This is useful to remove punctuation
+from a token, for example.
+"""
+function trimhead(s::AbstractString, arr)
+    if (isempty(s))
+        s
+    else
+        firstch = s[1]
+        if occursin(firstch,arr)
+            trimmed = s[2:end]
+            trimhead(trimmed, arr)
+        else
+            s
+        end
+    end
+end
+
+
+"""
+$(SIGNATURES)
 Remove all trailing characters belong to list in `arr`,
 and collect them.  This is useful to collect punctuation
 from a token, for example.
@@ -24,7 +71,7 @@ end
 
 """
 Remove all trailing characters belong to list in `arr`,
-and and return the trimmed string.  
+and return the trimmed string.  
 
 $(SIGNATURES)
 
--- test/test_generics.jl
+++ test/test_generics.jl
@@ -3,7 +3,7 @@
 
 @testset "Test collecting and stripping punctuation" begin
     s = "abcd;"
-    punct = ",:!.;"
+    punct = ",:!.;'"
     strippedaway = Orthography.collecttail(s,punct)
     @test strippedaway === ";"
 
@@ -12,4 +12,8 @@
 
     nopunct = Orthography.collecttail("abcd", punct)
     @test nopunct == ""
+
+    s2 = "'single quoted'"
+    @test Orthography.trimhead(s2,punct) == "single quoted'"
+    @test  Orthography.collecthead(s2,punct) == "'"
 end
